895e8a5730826cbf148daa9241286447d480f152
Lars Schneidenbach <schneidenbach@us.ibm.com>
2014-10-16 14:29:59 -0400

    enable CNK build config, cleanup of some linking

---
+++ CMake/Site/bgq_cnk.cmake
@@ -0,0 +1,6 @@
+set(MPI_CXX_COMPILER /bgsys/drivers/ppcfloor/comm/gcc/bin/mpicxx)
+set(MPI_C_COMPILER /bgsys/drivers/ppcfloor/comm/gcc/bin/mpicc)
+
+set(SKV_ENV "BGQCNK" )
+set(SKV_MPI "MPICH_BGQ" )
+
